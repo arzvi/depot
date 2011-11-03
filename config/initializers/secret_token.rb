6e43e97060ff239e4bbcd3816a6e51d21844bcdb
Arun Srinivasan <arunrocks@gmail.com>
2011-11-03 09:58:03 +0530

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '52f4fd38a8e1a48ca63c1dc21aea52037943f6344781f53fe38c3976ded684eff04a3a9b5fda55f434106707cbbe8265d1391dd3f8dcc56426417b3ab1e39410'
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+​Product.delete_all​
+
+Product.create(title: 'Programming Ruby 1.9',​ description:​ %{<p>​  Ruby is the fastest growing and most exciting dynamic language​ out there. If you need to get working programs delivered fast,​ you should add Ruby to your toolbox.​ </p>}, image_url: 'ruby.jpg',​price: 49.95)​
+
+
+
+
+
+
+
